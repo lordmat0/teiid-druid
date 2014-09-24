e974f778234d45022c58d23002d2fc1ed29f587a
matthew <lordmat@gmail.com>
2014-09-24 14:41:57 +0100

    Added debugging output for querying

--- druid/connector-druid/src/test/java/com/druid/connector_druidtest/TestClassConnector.java
+++ druid/connector-druid/src/test/java/com/druid/connector_druidtest/TestClassConnector.java
@@ -1,16 +1,15 @@
 package com.druid.connector_druidtest;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 import org.joda.time.DateTime;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import scala.Either;
@@ -26,12 +25,11 @@ import com.yahoo.sql4d.sql4ddriver.rowmapper.TimeSeriesBean;
 public class TestClassConnector {
 
 	private DruidConnection druidConnection;
-	
-    String timeseriesContentAggHourly = "SELECT timestamp , LONG_SUM(content_views) AS content_views, LONG_SUM(shares) AS shares FROM AggTable WHERE interval BETWEEN  2014-05-20T00:00:00.000-04:00 AND 2014-05-31T23:00:00.000-04:00 AND provider_id='superprovider' AND content_type='cavideo' BREAK BY PERIOD('P1D', 'EST5EDT')  GROUP BY timestamp, content_views, shares HINT('timeseries')";
-    String timeseriesUniqCount = "SELECT timestamp , LONG_SUM(all_content_seen) AS content_seen FROM UniqueCountTable WHERE interval BETWEEN  2014-05-20T00:00:00.000-04:00 AND 2014-05-31T23:00:00.000-04:00 AND provider_id='superprovider' AND content_type='cavideo' BREAK BY PERIOD('P1D', 'EST5EDT') GROUP BY timestamp,content_seen HINT('timeseries')";
-    String tsJoin = String.format("%s JOIN (%s) ON (timestamp)", timeseriesContentAggHourly, timeseriesUniqCount);
 
-	
+	String timeseriesContentAggHourly = "SELECT timestamp , LONG_SUM(content_views) AS content_views, LONG_SUM(shares) AS shares FROM AggTable WHERE interval BETWEEN  2014-05-20T00:00:00.000-04:00 AND 2014-05-31T23:00:00.000-04:00 AND provider_id='superprovider' AND content_type='cavideo' BREAK BY PERIOD('P1D', 'EST5EDT')  GROUP BY timestamp, content_views, shares HINT('timeseries')";
+	String timeseriesUniqCount = "SELECT timestamp , LONG_SUM(all_content_seen) AS content_seen FROM UniqueCountTable WHERE interval BETWEEN  2014-05-20T00:00:00.000-04:00 AND 2014-05-31T23:00:00.000-04:00 AND provider_id='superprovider' AND content_type='cavideo' BREAK BY PERIOD('P1D', 'EST5EDT') GROUP BY timestamp,content_seen HINT('timeseries')";
+	String tsJoin = String.format("%s JOIN (%s) ON (timestamp)",
+			timeseriesContentAggHourly, timeseriesUniqCount);
 
 	public TestClassConnector() {
 	}
@@ -46,7 +44,7 @@ public class TestClassConnector {
 	public void tearDown() throws Exception {
 	}
 
-	@Test
+	@Ignore
 	public void testTables() {
 		DDataSource source = druidConnection.getConnection();
 
@@ -63,26 +61,39 @@ public class TestClassConnector {
 		params.add("endT", new DateTime());
 		DDataSource source = druidConnection.getConnection();
 		source.setNamedParams(params);
+		String queryResult = source.query(sql, true, "sql").left().get();
+		assertEquals(queryResult, "test");
+	}
+
+	@Ignore
+	public void testQuery2() {
+		String sql = "SELECT page FROM wikipedia WHERE interval BETWEEN :startT AND :endT limit 5 ";
+		NamedParameters params = new NamedParameters();
+		params.add("startT", new DateTime(2014, 9, 1, 0, 0));
+		params.add("endT", new DateTime());
+		DDataSource source = druidConnection.getConnection();
+		source.setNamedParams(params);
 		String queryResult = source.query(sql, false, "sql").left().get();
 		assertEquals(queryResult, "test");
 	}
-	
-	
-	@Test
-	public void testComplexQuery() throws Exception{
+
+	@Ignore
+	public void testComplexQuery() throws Exception {
 		DDataSource source = druidConnection.getConnection();
 
-		
-        Either<String, Either<List<TimeSeriesBean>, Map<Object, TimeSeriesBean>>> mapperRes = source.query(tsJoin, TimeSeriesBean.class, false);
-        if (mapperRes.isLeft()) {
-            throw new Exception(mapperRes.left().get());
-        }
-        Either<List<TimeSeriesBean>, Map<Object, TimeSeriesBean>> goodResult = mapperRes.right().get();
-        if (goodResult.isLeft()) {
-            PrettyPrint.print(goodResult.left().get());
-        } else {
-            PrettyPrint.print(new ArrayList<>(goodResult.right().get().values()));
-        }
+		Either<String, Either<List<TimeSeriesBean>, Map<Object, TimeSeriesBean>>> mapperRes = source
+				.query(tsJoin, TimeSeriesBean.class, false);
+		if (mapperRes.isLeft()) {
+			throw new Exception(mapperRes.left().get());
+		}
+		Either<List<TimeSeriesBean>, Map<Object, TimeSeriesBean>> goodResult = mapperRes
+				.right().get();
+		if (goodResult.isLeft()) {
+			PrettyPrint.print(goodResult.left().get());
+		} else {
+			PrettyPrint
+					.print(new ArrayList<>(goodResult.right().get().values()));
+		}
 	}
 
 }
