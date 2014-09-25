fc33f55590ccad7fa70fb0039152b56489bdbbdb
matthew <lordmat@gmail.com>
2014-09-25 10:42:57 +0100

    Added handling of proxy IP and ports for connection

--- druid/connector-druid/src/main/java/com/druid/connector_druid/DruidConnectionImpl.java
+++ druid/connector-druid/src/main/java/com/druid/connector_druid/DruidConnectionImpl.java
@@ -3,7 +3,6 @@
  */
 package com.druid.connector_druid;
 
-
 import javax.resource.ResourceException;
 
 import org.teiid.resource.spi.BasicConnection;
@@ -13,34 +12,46 @@ import com.druid.translator_druid.DruidConnection;
 import com.yahoo.sql4d.sql4ddriver.DDataSource;
 
 /**
- * Connection to the resource. You must define druidConnection interface, that 
+ * Connection to the resource. You must define druidConnection interface, that
  * extends the "javax.resource.cci.Connection"
  */
-public class DruidConnectionImpl extends BasicConnection implements DruidConnection {
+public class DruidConnectionImpl extends BasicConnection implements
+		DruidConnection {
 
-    private DruidManagedConnectionFactory config;
-    private DDataSource connection;
+	private DruidManagedConnectionFactory config;
+	private DDataSource connection;
 
-    public DruidConnectionImpl(DruidManagedConnectionFactory env) {
-        this.config = env;
-        
-        String brokerIp = env.getBrokerNodeIp();
-        int brokerPort = env.getBrokerPort();
-        
-        String coordinatorIp = env.getCoordinatorIp();
-        int coordinatorPort = env.getCoordinatorPort();
-        
-        
-        this.connection = new DDataSource(brokerIp, brokerPort, coordinatorIp, coordinatorPort);
-        //this.connection = new DDataSource("192.168.30.170", 8080, "192.168.30.170", 8085);
-    }
-    
-    public DDataSource getConnection() {
-    	return connection;
-    }
-    
-    public void close() {
-    	
-    }
+	public DruidConnectionImpl(DruidManagedConnectionFactory env) {
+		this.config = env;
+
+		String brokerIp = env.getBrokerNodeIp();
+		int brokerPort = env.getBrokerPort();
+
+		String coordinatorIp = env.getCoordinatorIp();
+		int coordinatorPort = env.getCoordinatorPort();
+
+		String proxyIp = env.getProxyIp();
+		int proxyPort = env.getBrokerPort();
+
+		if (proxyIp == null || proxyIp.equals("") || proxyPort == 0) {
+			// No proxy
+			this.connection = new DDataSource(brokerIp, brokerPort,
+					coordinatorIp, coordinatorPort);
+		} else {
+			this.connection = new DDataSource(brokerIp, brokerPort,
+					coordinatorIp, coordinatorPort, proxyIp, proxyPort);
+		}
+
+		// this.connection = new DDataSource("192.168.30.170", 8080,
+		// "192.168.30.170", 8085);
+	}
+
+	public DDataSource getConnection() {
+		return connection;
+	}
+
+	public void close() {
+
+	}
 
 }
--- druid/connector-druid/src/main/java/com/druid/connector_druid/DruidManagedConnectionFactory.java
+++ druid/connector-druid/src/main/java/com/druid/connector_druid/DruidManagedConnectionFactory.java
@@ -9,27 +9,31 @@ import org.teiid.core.BundleUtil;
 import org.teiid.resource.spi.BasicConnectionFactory;
 import org.teiid.resource.spi.BasicManagedConnectionFactory;
 
-public class DruidManagedConnectionFactory extends BasicManagedConnectionFactory {
+public class DruidManagedConnectionFactory extends
+		BasicManagedConnectionFactory {
 
 	private static final long serialVersionUID = -1437528038325708535L;
-	
-	public static final BundleUtil UTIL = BundleUtil.getBundleUtil(DruidManagedConnectionFactory.class);
-	
-	
+
+	public static final BundleUtil UTIL = BundleUtil
+			.getBundleUtil(DruidManagedConnectionFactory.class);
+
 	private String brokerNodeIp;
 	private int brokerPort;
-	
+
 	private String coordinatorIp;
 	private int coordinatorPort;
-	
-	
-	
+
+	private String proxyIp;
+	private String proxyPort;
+
 	@Override
-	public BasicConnectionFactory<DruidConnectionImpl> createConnectionFactory() throws ResourceException {
+	public BasicConnectionFactory<DruidConnectionImpl> createConnectionFactory()
+			throws ResourceException {
 		return new BasicConnectionFactory<DruidConnectionImpl>() {
 			@Override
 			public DruidConnectionImpl getConnection() throws ResourceException {
-				return new DruidConnectionImpl(DruidManagedConnectionFactory.this);
+				return new DruidConnectionImpl(
+						DruidManagedConnectionFactory.this);
 			}
 		};
 	}
@@ -40,45 +44,48 @@ public class DruidManagedConnectionFactory extends BasicManagedConnectionFactory
 		return brokerNodeIp;
 	}
 
-
-
 	public void setBrokerNodeIp(String brokerNodeIp) {
 		this.brokerNodeIp = brokerNodeIp;
 	}
-
-
 
 	public int getBrokerPort() {
 		return brokerPort;
 	}
 
-
-
 	public void setBrokerPort(int brokerPort) {
 		this.brokerPort = brokerPort;
 	}
-
-
 
 	public String getCoordinatorIp() {
 		return coordinatorIp;
 	}
 
-
-
 	public void setCoordinatorIp(String coordinatorIp) {
 		this.coordinatorIp = coordinatorIp;
 	}
-
-
 
 	public int getCoordinatorPort() {
 		return coordinatorPort;
 	}
 
-
-
 	public void setCoordinatorPort(int coordinatorPort) {
 		this.coordinatorPort = coordinatorPort;
-	}	
+	}
+
+	public String getProxyIp() {
+		return proxyIp;
+	}
+
+	public void setProxyIp(String proxyIp) {
+		this.proxyIp = proxyIp;
+	}
+
+	public String getProxyPort() {
+		return proxyPort;
+	}
+
+	public void setProxyPort(String proxyPort) {
+		this.proxyPort = proxyPort;
+	}
+
 }
